429decfb49bf5b2844f742438f1297498297c70e
称明 <8456918+Windorain@users.noreply.github.com>
2026-02-23 00:25:47 +0800

    使代码干净一些

--- src/main/java/com/github/wohaopa/MyCTMLib/render/phases/PrepareElementDataPhase.java
+++ src/main/java/com/github/wohaopa/MyCTMLib/render/phases/PrepareElementDataPhase.java
@@ -1,5 +1,7 @@
 package com.github.wohaopa.MyCTMLib.render.phases;
 
+import net.minecraft.util.IIcon;
+
 import com.github.wohaopa.MyCTMLib.model.ModelElement;
 import com.github.wohaopa.MyCTMLib.model.ModelFace;
 import com.github.wohaopa.MyCTMLib.render.CTMRenderEntry;
@@ -15,42 +17,62 @@ public class PrepareElementDataPhase implements PipelinePhase {
     @Override
     public PhaseResult process(RenderContext context) {
         ModelElement element = context.getCurrentElement();
-        if (element != null) {
-            ModelFace faceData = element.getFace(context.getFace());
-            if (faceData != null && faceData.getTextureKey() != null) {
-                String modelId = context.getModelId();
-                String domain = modelId != null && modelId.indexOf(':') >= 0 
-                    ? modelId.substring(0, modelId.indexOf(':')) 
-                    : "minecraft";
-                
-                String texturePath = TextureKeyNormalizer.resolveTexturePath(
-                    faceData.getTextureKey(), 
-                    context.getModelData().getTextures());
-                
-                if (texturePath != null) {
-                    String textureLookupKey = TextureKeyNormalizer.toCanonicalTextureKey(domain, texturePath);
-                    context.setTextureData(CTMRenderEntry.getConnectingData(textureLookupKey));
-                    
-                    if (context.getDrawIcon() == null) {
-                        context.setDrawIcon(TextureRegistry.getInstance().getIcon(textureLookupKey));
-                    }
-                    if (context.getDrawIcon() == null) {
-                        context.setDrawIcon(context.getOriginalIcon());
-                    }
-                    
-                    float[] f = element.getFrom(), t = element.getTo();
-                    context.setDrawRelMinX(Math.min(f[0], t[0]) / 16.0);
-                    context.setDrawRelMaxX(Math.max(f[0], t[0]) / 16.0);
-                    context.setDrawRelMinY(Math.min(f[1], t[1]) / 16.0);
-                    context.setDrawRelMaxY(Math.max(f[1], t[1]) / 16.0);
-                    context.setDrawRelMinZ(Math.min(f[2], t[2]) / 16.0);
-                    context.setDrawRelMaxZ(Math.max(f[2], t[2]) / 16.0);
-                }
-            }
+        if (element == null) {
+            return continuePhase(context);
         }
+        
+        ModelFace faceData = element.getFace(context.getFace());
+        if (faceData == null || faceData.getTextureKey() == null) {
+            return continuePhase(context);
+        }
+        
+        String modelId = context.getModelId();
+        String domain = extractDomain(modelId);
+        
+        String texturePath = TextureKeyNormalizer.resolveTexturePath(
+            faceData.getTextureKey(), 
+            context.getModelData().getTextures());
+        
+        if (texturePath == null) {
+            return continuePhase(context);
+        }
+        
+        String textureKey = TextureKeyNormalizer.toCanonicalTextureKey(domain, texturePath);
+        context.setTextureData(CTMRenderEntry.getConnectingData(textureKey));
+        
+        IIcon drawIcon = TextureRegistry.getInstance().getIcon(textureKey);
+        if (drawIcon == null) {
+            drawIcon = context.getOriginalIcon();
+        }
+        context.setDrawIcon(drawIcon);
+        
+        setBoundsFromElement(context, element);
         
         context.popState();
         context.pushState(RenderState.CALCULATE_TEXTURE);
         return PhaseResult.CONTINUE;
+    }
+    
+    private PhaseResult continuePhase(RenderContext context) {
+        context.popState();
+        context.pushState(RenderState.CALCULATE_TEXTURE);
+        return PhaseResult.CONTINUE;
+    }
+    
+    private String extractDomain(String modelId) {
+        if (modelId == null || modelId.indexOf(':') < 0) {
+            return "minecraft";
+        }
+        return modelId.substring(0, modelId.indexOf(':'));
+    }
+    
+    private void setBoundsFromElement(RenderContext context, ModelElement element) {
+        float[] f = element.getFrom(), t = element.getTo();
+        context.setDrawRelMinX(Math.min(f[0], t[0]) / 16.0);
+        context.setDrawRelMaxX(Math.max(f[0], t[0]) / 16.0);
+        context.setDrawRelMinY(Math.min(f[1], t[1]) / 16.0);
+        context.setDrawRelMaxY(Math.max(f[1], t[1]) / 16.0);
+        context.setDrawRelMinZ(Math.min(f[2], t[2]) / 16.0);
+        context.setDrawRelMaxZ(Math.max(f[2], t[2]) / 16.0);
     }
 }
